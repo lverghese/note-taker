3fce2c1a0699ac67898c8f5d0f6d3ccc3d1f8ec3
lverghese <loganverghese@gmail.com>
2021-09-05 14:17:18 -0400

    modularized routes and sample note

--- routes/apiRoutes/saveRoutes.js
+++ routes/apiRoutes/saveRoutes.js
@@ -9,6 +9,24 @@ router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../../db/db.json"))
 });
 
+//post receive new note to save, add to json, and return to user
+router.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    console.log(noteList)
+    let noteListLength = (noteList.notes.length).toString();
+    
+    //this is where the id for each note
+    newNote.id = noteListLength;
+  
+    //push new or updated note to db.json data
+    noteList.notes.push(newNote);
+  
+    //writes new data to json file and shows updated note
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  
+    res.json(noteList);
+  });
 
 
 
--- server.js
+++ server.js
@@ -17,26 +17,6 @@ app.use('/', htmlRoutes);
  app.use('', apiRoutes);
 
 
-//post receive new note to save, add to json, and return to user
-app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-  console.log(noteList)
-  let noteListLength = (noteList.notes.length).toString();
-  
-  //this is where the id for each note
-  newNote.id = noteListLength;
-
-  //push new or updated note to db.json data
-  noteList.notes.push(newNote);
-
-  //writes new data to json file and shows updated note
-  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-
-  res.json(noteList);
-});
-
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
